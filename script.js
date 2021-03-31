726a20130076045d131a5cfffb007cf285c88245
schirmann <cschirmann.dev@gmail.com>
2021-03-31 15:09:49 -0300

    fetch async await

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-import { somar } from "./mat.js";
+function requisitar () {
+  const url = 'https://alunos.b7web.com.br/api/ping';
+  const params = {
+    method:'POST',
+    body:JSON.stringify({
+      nome:'Cintia',
+      idade:30
+    })
+  },
+ 
+  fetch(url, params)
+  .then((r)=>r.json())
+  .then((json)=>{
+    console.log(json);
+  });
 
-console.log(somar(10, 5));
+  console.log('alguma coisa');
+
+}
+
+async function requisitar2() {
+  const url = 'https://alunos.b7web.com.br/api/ping';
+  const params = {
+    method:'POST',
+    body:JSON.stringify({
+      nome:'Cintia',
+      idade:30
+    })
+  },
+}
